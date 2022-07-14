96e70abe94b9d0f18885cb9b971de5345e3bf9d4
Sean Oh <seanoh@ucsb.edu>
2022-07-13 23:14:11 -0700

    add prompt questions

---
+++ YOUR_README.md
@@ -0,0 +1,41 @@
+
+# Cool Project
+
+undefined
+
+## Description 
+undefined
+
+
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+undefined
+* [Contributing](#contributing)
+* [Tests](#tests)
+
+
+## Installation
+undefined
+
+## Usage 
+undefined
+
+## Credits
+undefined
+
+undefined
+
+## Contributing
+undefined
+
+## Tests
+undefined
+
+
+
+
+
--- index.js
+++ index.js
@@ -8,7 +8,53 @@ const questions = [
   {
     name: "title",
     type: "input",
-    message: "Please input the name of your project:"
+    message: "Please enter the name of your project:"
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "Please enter a description for your project:"
+  },
+  {
+    name: "deployedLink",
+    type: "input",
+    message: "Please enter the URL to your deployed site. If your project isn't deployed, leave this blank:"
+  },
+  {
+    name: "installation",
+    type: "input",
+    message: "Please enter installation instructions:"
+  },
+  {
+    name: "usage",
+    type: "input",
+    message: "Please enter usage information:"
+  },
+  {
+    name: "credits",
+    type: "input",
+    message: "Please enter any credits:"
+  },
+  {
+    name: "license",
+    type: "list",
+    message: "Please select a license from the list:",
+    choices: []
+  },
+  {
+    name: "contributing",
+    type: "input",
+    message: "Please enter contribution guidelines:"
+  },
+  {
+    name: "username",
+    type: "input",
+    message: "Please enter your Github username:"
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Please enter your email:"
   },
   
 ];
